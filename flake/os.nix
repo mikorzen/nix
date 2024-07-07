accbaeb7c6ac38b081c2a227ec5eb91f7b5c5c4b
mikorzen <michal.korzen@proton.me>
2024-07-07 08:34:18 +0200

    q

--- flake/os.nix
+++ flake/os.nix
@@ -2,14 +2,20 @@
   "Acerussy" = nixpkgs.lib.nixosSystem {
     specialArgs = { inherit inputs system; };
     modules = [
-      ../hosts/Acerussy.nix
+    ../common/os.nix                 # user-agnostic     host-agnostic     NixOS configuration
+    ../hosts/Acerussy/hardware.nix   # user-agnostic     Acerussy-specific NixOS hardware configuration
+    ../hosts/Acerussy/os.nix         # user-agnostic     Acerussy-specific NixOS configuration
+    ../users/mikorzen/os.nix         # mikorzen-specific host-agnostic     NixOS configuration
     ];
   };
 
   "Computerussy" = nixpkgs.lib.nixosSystem {
     specialArgs = { inherit inputs system; };
     modules = [
-      ../hosts/Computerussy.nix
+      ../common/os.nix                     # user-agnostic     host-agnostic         NixOS configuration
+      ../hosts/Computerussy/hardware.nix   # user-agnostic     Computerussy-specific NixOS hardware configuration
+      ../hosts/Computerussy/os.nix         # user-agnostic     Computerussy-specific NixOS configuration
+      ../users/mikorzen/os.nix             # mikorzen-specific host-agnostic         NixOS configuration
     ];
   };
 }
