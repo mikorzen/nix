03db09d3f3b736e4ded19958f06513580181a8aa
Michał Korzeń <michal.korzen-tech@pm.me>
2025-06-29 19:18:58 +0200

    Add files via upload

---
+++ disko.nix
@@ -0,0 +1,52 @@
+{
+  disko.devices = {
+    disk.sda = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            priority = 1;
+            name = "EFI System Partition";
+            label = "ESP";
+            type = "EF00";
+            size = "1G";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          root = {
+            priority = 2;
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-L" "nixos" "-f" ];
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                  mountOptions = [ "subvol=@" "compress=zstd" "noatime" ];
+                };
+                "/home" = {
+                  mountpoint = "/home";
+                  mountOptions = [ "subvol=@home" "compress=zstd" "noatime" ];
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "subvol=@nix" "compress=zstd" "noatime" ];
+                };
+                "/swap" = {
+                  mountpoint = "/swap";
+                  swap.swapfile.size = "8G";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
