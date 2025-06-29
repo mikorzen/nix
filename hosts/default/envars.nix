48881b8dc60b798666f368cc62befd3f6839103d
Michał Korzeń <michal.korzen-tech@pm.me>
2025-06-29 19:18:21 +0200

    Delete hosts directory

--- hosts/default/envars.nix
+++
@@ -1,6 +0,0 @@
-{
-  environment.variables = {  ## use wayland for Electron apps
-    QT_QPA_PLATFORM = "wayland";
-    NIXOS_OZONE_WL = "1";
-  };
-}
--- hosts/default/software/virtualisation.nix
+++
@@ -1,6 +0,0 @@
-{
-  imports = [
-    ./virtualisation/containers.nix
-    ./virtualisation/virtual-machines.nix
-  ];
-}
