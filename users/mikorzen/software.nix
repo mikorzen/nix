e513589a1535932c07423f13b3ca38cbd34bd6d0
mikorzen <michal.korzen-tech@pm.me>
2025-07-02 17:18:37 +0200

    enable gnome extensions in a specific order

--- users/mikorzen/software.nix
+++ users/mikorzen/software.nix
@@ -2,6 +2,7 @@
   imports = [
     ./software/development.nix
     ./software/entertainment.nix
+    ./software/gnome-extensions.nix
     ./software/media.nix
     ./software/terminal.nix
     ./software/utilities.nix
--- users/mikorzen/software/gnome-extensions.nix
+++ users/mikorzen/software/gnome-extensions.nix
@@ -1,4 +1,31 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  setup-extensions = pkgs.writeShellScript "setup-extensions" ''
+    mapfile extensions -t < ~/.config/gnome-extensions/extension-order
+
+    dconf write /org/gnome/shell/disabled-extensions "'[]'"
+    dconf write /org/gnome/shell/enabled-extensions "'[]'"
+
+    # shellcheck disable=SC2068
+    for e in ''${extensions[@]}; do
+        gnome-extensions enable "$e"
+        sleep 0.1
+    done
+  '';
+in {
+  systemd.user.services.setup-extensions = {
+    Unit = {
+      Description = "Disable and re-enable GNOME extensions in a specific order";
+    };
+    Install = {
+      WantedBy = [ "org.gnome.Shell.target" ];
+    };
+    Service = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${setup-extensions}";
+    };
+  };
+
   home.packages = with pkgs.gnomeExtensions; [
     advanced-alttab-window-switcher
     appindicator
@@ -12,12 +39,10 @@
     # disconnect-wifi  # not available for GNOME 48
     emoji-copy
     fuzzy-app-search
-    gnome-40-ui-improvements
     just-perfection
     middle-click-to-close-in-overview
     panel-corners
     pip-on-top
-    quick-settings-audio-panel
     # quick-settings-tweaker  # broken on GNOME 48 https://github.com/qwreey/quick-settings-tweaks/issues/197#issuecomment-2913560783
     reboottouefi
     transparent-window-moving
